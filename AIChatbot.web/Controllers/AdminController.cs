1e45ba23754993b307a74b56d0767c623fa50a32
PARASH TAMANG <parashlearning@gmail.com>
2026-03-17 17:19:01 +0530

    create a dynamic database setup page

---
+++ AIChatbot.web/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AIChatbot.web.Controllers
+{
+    public class AdminController : Controller
+    {
+            public IActionResult Dashboard()
+            {
+                return View();
+            }
+
+            public IActionResult DatabaseConnections()
+            {
+                return View();
+            }
+
+            public IActionResult ConfigureDatabases()
+            {
+                return View();
+            }
+
+            public IActionResult KnowledgeBases()
+            {
+                return View();
+            }
+
+            public IActionResult Settings()
+            {
+                return View();
+            }
+    }
+}
